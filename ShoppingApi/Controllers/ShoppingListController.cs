b63e9dd61387e23adc8562f0a6edde6834b2d619
Jeffry Gonzalez <jeff@hypertheory.com>
2022-02-28 16:04:36 -0500

    added post

--- ShoppingApi/Controllers/ShoppingListController.cs
+++ ShoppingApi/Controllers/ShoppingListController.cs
@@ -14,16 +14,35 @@ public class ShoppingListController : ControllerBase
 
     // GET /shopping-list
     [HttpGet("shopping-list")]
-    public ActionResult GetShoppingList()
+    public async Task<ActionResult> GetShoppingList()
     {
-        var response = _context.Items!
+        var response = await _context.Items!
             .Where(i=> i.Removed == false)
             .Select(i => new ShoppingListItem(i.Id.ToString(), i.Description, i.Purchased))
-            .ToList();
+            .ToListAsync();
         return Ok(new { data = response});
     }
 
+    [HttpPost("shopping-list")]
+    public async Task<ActionResult> AddShoppingItem([FromBody] PostShoppingListRequest request)
+    {
+        // 1. validate the thing.
+        var item = new ShoppingItem
+        {
+            Description = request.description,
+            Purchased = false,
+            WhenAdded = DateTime.Now,
+            Removed = false
+        };
+        _context.Items!.Add(item);
+        await _context.SaveChangesAsync(); // actually save it.
+
+        var response = new ShoppingListItem(item.Id.ToString(), item.Description, item.Purchased);
+        return Ok(response);
+    }
 
 }
 
 public record ShoppingListItem(string id, string description, bool purchased);
+
+public record PostShoppingListRequest(string description);
